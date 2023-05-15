70192fd763e78fb9f8585a4ad57c0250da04b42d
Zinedine Tomasella <zinedine.tomasella@hispeed.ch>
2023-05-15 16:06:06 +0200

    change

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -9,19 +9,28 @@
   false
 ); */
 
-// Scrolls um 100vh
-/* window.addEventListener('wheel', event => {
-  if (event.deltaY > 0) {
-    window.scrollBy(0, window.innerHeight);
-  }
+// Active Klasse der Navbar geben so weiss man wo man ist
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav ul li');
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (window.scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLi.forEach(li => {
+    li.classList.remove('active');
+    if (li.classList.contains(current)) {
+      li.classList.add('active');
+    }
+  });
 });
 
-window.addEventListener('wheel', event => {
-  if (event.deltaY < 0) {
-    window.scrollBy(0, -window.innerHeight);
-  }
-}); */
-
+// Auto snap 100vh
 window.addEventListener('scroll', e => {
   let scrolled = window.pageYOffset;
   setTimeout(() => {
@@ -35,9 +44,11 @@ window.addEventListener('scroll', e => {
   }, 350);
 });
 
+//Animation when 50% is visible
 const hiddenLeft = document.querySelectorAll('.hiddenLeft');
 const hiddenRight = document.querySelectorAll('.hiddenRight');
 const hiddenUnder = document.querySelectorAll('.hiddenUnder');
+const hiddenAbove = document.querySelectorAll('.hiddenAbove');
 
 const options = {
   root: null,
@@ -50,13 +61,13 @@ const observer = new IntersectionObserver(entries => {
     if (entry.isIntersecting) {
       if (entry.target.classList.contains('hiddenLeft') || entry.target.classList.contains('hiddenRight')) {
         entry.target.classList.add('slideIn');
-      } else if (entry.target.classList.contains('hiddenUnder')) {
+      } else if (entry.target.classList.contains('hiddenUnder') || entry.target.classList.contains('hiddenAbove')) {
         entry.target.classList.add('slideInUp');
       }
     } else {
       if (entry.target.classList.contains('hiddenLeft') || entry.target.classList.contains('hiddenRight')) {
         entry.target.classList.remove('slideIn');
-      } else if (entry.target.classList.contains('hiddenUnder')) {
+      } else if (entry.target.classList.contains('hiddenUnder') || entry.target.classList.contains('hiddenAbove')) {
         entry.target.classList.remove('slideInUp');
       }
     }
@@ -66,8 +77,10 @@ const observer = new IntersectionObserver(entries => {
 hiddenLeft.forEach(element => observer.observe(element));
 hiddenRight.forEach(element => observer.observe(element));
 hiddenUnder.forEach(element => observer.observe(element));
+hiddenAbove.forEach(element => observer.observe(element));
 
-const contactForm = document.querySelector('.contact');
+// Send Emails from a formular
+const contactForm = document.querySelector('.contact-form');
 let namen = document.querySelector('.text-box-name');
 let email = document.querySelector('.text-box-email');
 let subject = document.querySelector('.text-box-subject');
@@ -90,6 +103,10 @@ contactForm.addEventListener('submit', e => {
     console.log(xhr.responseText);
     if (xhr.responseText == 'Success') {
       /*       document.querySelector('#alert-success').style.display = 'block'; */
+      namen.value = '';
+      email.value = '';
+      subject.value = '';
+      message.value = '';
     } /* else {
       document.querySelector('#alert-error').style.display = 'block';
     } */
@@ -97,3 +114,37 @@ contactForm.addEventListener('submit', e => {
 
   xhr.send(JSON.stringify(formData));
 });
+
+// Slideshow working
+/* let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+} */
+
+// Thumbnail image controls
+/* function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+} */
